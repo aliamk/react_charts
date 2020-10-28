59546c1f99697eb1a94a1aae66220f1495ceb576
ALIA <malek.alia@gmail.com>
2020-10-28 17:08:51 +0000

    created component folder an added navbar item files

---
+++ src/component/DankMemes.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DankMemes = () => {
+  return (
+    <div className="App">
+      <h1>More Dank Memes</h1>
+    </div>
+  );
+};
+
+export default DankMemes;
---
+++ src/component/MoreDeets.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MoreDeets = () => {
+  return (
+    <div>
+      <h1>More Deets</h1>
+    </div>
+  );
+};
+
+export default MoreDeets;
