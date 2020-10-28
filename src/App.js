9f4a20a52a0a56af309416a5b5b675b9cb02e011
ALIA <malek.alia@gmail.com>
2020-10-28 17:49:48 +0000

    updated the features component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import React from "react";
 import "./App.css";
-import * as ReactBootStrap from "react-bootstrap";
 import DankMemes from "./component/DankMemes";
 import Features from "./component/Features";
 import MoreDeets from "./component/MoreDeets";
 import Navbar from "./component/Navbar";
 import Pricing from "./component/Pricing";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <div className="App">
-      <Navbar />
+      <Router>
+        <Navbar />
+        {/* A <Switch> looks through its children <Route>s and renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/features" component={Features}>
+            <Features />
+          </Route>
+          <Route path="/pricing" component={Pricing}>
+            <Pricing />
+          </Route>
+          <Route path="/moredeets" component={MoreDeets}>
+            <MoreDeets />
+          </Route>
+          <Route path="/dankmemes" component={DankMemes}>
+            <DankMemes />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 };
