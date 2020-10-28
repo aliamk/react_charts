df797074e683abecb307590cf19d28520274000c
ALIA <malek.alia@gmail.com>
2020-10-28 17:48:21 +0000

    install react-router and connected the component links to the navbar

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,17 +1,24 @@
 import React from "react";
 import "../App.css";
 import * as ReactBootStrap from "react-bootstrap";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div className="App">
       <ReactBootStrap.Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <ReactBootStrap.Navbar.Brand href="#home">Charts</ReactBootStrap.Navbar.Brand>
+        <Link to="/home">
+          <ReactBootStrap.Navbar.Brand href="#home">CHARTS</ReactBootStrap.Navbar.Brand>
+        </Link>
         <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav">
           <ReactBootStrap.Nav className="mr-auto">
-            <ReactBootStrap.Nav.Link href="#features">Features</ReactBootStrap.Nav.Link>
-            <ReactBootStrap.Nav.Link href="#pricing">Pricing</ReactBootStrap.Nav.Link>
+            <Link to="/features">
+              <ReactBootStrap.Nav.Link href="#features">Features</ReactBootStrap.Nav.Link>
+            </Link>
+            <Link to="/pricing">
+              <ReactBootStrap.Nav.Link href="#pricing">Pricing</ReactBootStrap.Nav.Link>
+            </Link>
             <ReactBootStrap.NavDropdown title="Dropdown" id="collasible-nav-dropdown">
               <ReactBootStrap.NavDropdown.Item href="#action/3.1">Action</ReactBootStrap.NavDropdown.Item>
               <ReactBootStrap.NavDropdown.Item href="#action/3.2">Another action</ReactBootStrap.NavDropdown.Item>
@@ -21,10 +28,14 @@ const Navbar = () => {
             </ReactBootStrap.NavDropdown>
           </ReactBootStrap.Nav>
           <ReactBootStrap.Nav>
-            <ReactBootStrap.Nav.Link href="#deets">More deets</ReactBootStrap.Nav.Link>
-            <ReactBootStrap.Nav.Link eventKey={2} href="#memes">
-              Dank memes
-            </ReactBootStrap.Nav.Link>
+            <Link to="/moredeets">
+              <ReactBootStrap.Nav.Link href="#deets">More deets</ReactBootStrap.Nav.Link>
+            </Link>
+            <Link to="/dankmemes">
+              <ReactBootStrap.Nav.Link eventKey={2} href="#memes">
+                Dank memes
+              </ReactBootStrap.Nav.Link>
+            </Link>
           </ReactBootStrap.Nav>
         </ReactBootStrap.Navbar.Collapse>
       </ReactBootStrap.Navbar>
