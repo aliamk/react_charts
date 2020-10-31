519903423ce550b1d9246e3699af9f70a29b0cae
ALIA <malek.alia@gmail.com>
2020-10-31 00:04:38 +0000

    added a Line Chart from chart.js using hard-coded data and useStat and useEffect

--- src/component/DankMemes.js
+++ src/component/DankMemes.js
@@ -1,9 +1,65 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Line } from "react-chartjs-2";
+import "./DankMemes.css";
 
 const DankMemes = () => {
+  // useState Hook
+  const [chartData, setChartData] = useState({});
+
+  // Initial State
+  const chart = () => {
+    setChartData({
+      labels: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+      datasets: [
+        {
+          label: "level of thiccness",
+          data: [33, 45, 12, 76, 69],
+          backgroundColor: ["rgba(75, 192, 192, 0.6)"],
+          borderWidth: 4,
+        },
+      ],
+    });
+  };
+
+  // useEffect Hook
+  useEffect(() => {
+    chart();
+  }, []);
+
+  // Render
   return (
-    <div className="App">
-      <h1>More Dank Memes</h1>
+    <div className="App-chart">
+      <h1 style={{ position: "absolute", top: "1em", marginTop: "1em" }}>More Dank Memes</h1>
+      <div style={{ minHeight: "30vh", height: "50vh", minWidth: "30vw", width: "50vw", marginTop: "10em" }}>
+        <Line
+          data={chartData}
+          options={{
+            responsive: true,
+            title: { text: "THICCNESS SCALE", display: true },
+            scales: {
+              yAxes: [
+                {
+                  ticks: {
+                    autoSkip: true,
+                    maxTicksLimit: 10,
+                    beginAtZero: true,
+                  },
+                  gridLines: {
+                    display: false,
+                  },
+                },
+              ],
+              xAxes: [
+                {
+                  gridLines: {
+                    display: false,
+                  },
+                },
+              ],
+            },
+          }}
+        />
+      </div>
     </div>
   );
 };
